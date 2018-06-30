33386656a36da39af82d9f22b161135290550884
RainChen <hirainchen@gmail.com>
2018-06-30 11:00:27 +0800

    show selected image info in new page

--- public/js/app.js
+++ public/js/app.js
@@ -30,8 +30,57 @@
   App.newPage = function () {
     console.info('newPage init');
 
+    App.newPage.initFileSelect();
+
     // init submit button
     $("#submitBtn").click(App.submitContent);
+  };
+
+  App.newPage.initFileSelect = function () {
+    // handle file select
+    function handleFileSelect (evt) {
+      var files = evt.target.files; // FileList object
+
+      // files is a FileList of File objects. List some properties.
+      var output = [];
+      for (var i = 0, f; f = files[i]; i++) {
+        // Only process image files.
+        if (!f.type.match('image.*')) {
+          continue;
+        }
+
+        // show selected file info
+        var tmpl = $.templates("#fileInfoTpl");
+        var data = {
+          name: escape(f.name),
+          type: (f.type || 'n/a'),
+          size: f.size,
+          modified: (f.lastModifiedDate ? f.lastModifiedDate.toLocaleDateString() : 'n/a')
+        };
+        var html = tmpl.render(data);
+        $("#fileInfo").html(html);
+
+
+        // read file content as base64
+        var reader = new FileReader();
+
+        // Closure to capture the file information.
+        reader.onload = (function(theFile) {
+          return function(e) {
+            // Render thumbnail.
+            var span = document.createElement('span');
+            span.innerHTML = ['<img class="thumb" src="', e.target.result,
+                              '" title="', escape(theFile.name), '"/>'].join('');
+            document.getElementById('fileInfo').insertBefore(span, null);
+          };
+        })(f);
+
+        // Read in the image file as a data URL.
+        reader.readAsDataURL(f);
+      }
+
+    }
+    document.getElementById('momentFile').addEventListener('change', handleFileSelect, false);
   };
 
   App.newMoment = function (data) {
